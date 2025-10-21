6aebd395032c98c96301b6b8d0e2839554456d09
Richard <richard.sinaga@berca.co.id>
2025-10-21 14:27:56 +0700

    add column name

--- internal/domain/user/entity.go
+++ internal/domain/user/entity.go
@@ -6,7 +6,7 @@ type User struct {
 	ID        uint      `gorm:"primaryKey" json:"id"`
 	Name      string    `gorm:"size:100;not null" json:"name"`
 	Email     string    `gorm:"size:255;uniqueIndex;not null" json:"email"`
-	Password  string    `gorm:"size:255;not null" json:"-"` // json:"-" untuk hide di response
+	Password  string    `gorm:"column:password_hash;size:255;not null" json:"-"` // json:"-" untuk hide di response
 	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updated_at"`
 }
